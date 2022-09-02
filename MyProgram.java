3764981f06c387dda7db53f35016f622073ab150
Abhishekshw <102210901+Abhishekshw@users.noreply.github.com>
2022-09-02 13:59:51 +0530

    Update MyProgram.java

--- MyProgram.java
+++ MyProgram.java
@@ -1,12 +1,12 @@
 class Displaymessage{
-	static void printMessage(){
+	static void printMessage(){//creating method
 		System.out.println("Hello My Message");
 	}
 }
 class  MyProgram {
 	  public static void main(String arg[]){
-		  Displaymessage program=new Displaymessage();
-		  program.printMessage();
+		  Displaymessage program=new Displaymessage();//creating object of class
+		  program.printMessage();//calling methid
 	 }
 
 	 
