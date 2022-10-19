c3007d69bfe096ed99b3e015fc1fea5d3a3adc83
Abhishekshw <102210901+Abhishekshw@users.noreply.github.com>
2022-10-19 14:35:56 +0530

    Update Exceptions.java

--- Exceptions.java
+++ Exceptions.java
@@ -1,22 +1,25 @@
 package Exeption;
-
-import java.util.Scanner;
+/*You will be given two integers x and y as input, you have to compute x/y. If x and y are 
+not 32 bit signed integers or if y is zero, exception will occur and you have to report it. 
+Read sample Input/Output to know what to report in case of exceptions.
+*/
+import java.util.Scanner;//importing scanner class
 
 public class Exceptions {
-	static void Operation() {
+	static void Operation() {//Ststicv method
 		Scanner S = new Scanner(System.in);
 		System.out.println("Enter 1st no");
-		int x = S.nextInt();
+		int x = S.nextInt();//Asking value
 		System.out.println("enter 2nd no");
 		int y = S.nextInt();
-		try {
+		try {//try block
 			System.out.println(x / y);
-		} catch (Exception e) {
-			System.out.println(e);
+		} catch (Exception e) {//catch exeption
+			System.out.println(e);//Showing Exeption
 		}
 	}
 
 	public static void main(String[] args) {
-		Operation();
+		Operation();//calling method
 	}
 }
